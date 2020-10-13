2a42233146ef1913a573656c2f366d50ef8e29ad
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-10-13 19:56:03 +0200

    Added toggle element visibility

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault()
 
+  document.querySelector('#message-panel').classList.add('hide')
+  document.querySelector('#link-panel').classList.remove('hide')
+
   const messageInput = document.querySelector('#message-input')
 
   const encrypted = btoa(messageInput.value)
